59ca45c164c7642aaedcec8229bc7bb7b4c29db6
ibnaleem <ibnaleem@outlook.com>
2026-03-08 00:38:13 +0000

    feat(memtable): implement Less method on Entry to satisfy btree.Item interface

--- memtable/memtable.go
+++ memtable/memtable.go
@@ -1,6 +1,20 @@
 package memtable
 
+import (
+	"bytes"
+
+	"github.com/google/btree"
+)
+
 type Entry struct {
 	Key []byte
 	Value []byte
+}
+
+func (e Entry) Less(other btree.Item) bool {
+
+	comparison := bytes.Compare(e.Key, other.(Entry).Key)
+
+	return comparison < 0
+
 }
