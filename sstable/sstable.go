17c75731d6cea475de731c591f990fe3895f0724
ibnaleem <ibnaleem@outlook.com>
2026-03-08 20:33:02 +0000

    feat(sstable): implement Write method with length-prefixed binary encoding

--- sstable/sstable.go
+++ sstable/sstable.go
@@ -1,10 +1,20 @@
 package sstable
 
 import (
+	"os"
 	"time"
+	"bufio"
+	"encoding/binary"
 
 	"github.com/bits-and-blooms/bloom"
+	"github.com/ibnaleem/golsm/memtable"
 )
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 type SSTable struct {
 	path        string
@@ -14,7 +24,7 @@ type SSTable struct {
 
 func New(path string) *SSTable {
 	now := time.Now()
-	
+
 	n := uint(1000000)
 	fp := 0.01 // 1% false positive rate
 
@@ -26,4 +36,32 @@ func New(path string) *SSTable {
 		bloomFilter: bf,
 	}
 
+}
+
+func (s *SSTable) Write(entries []memtable.Entry) {
+
+	f, err := os.OpenFile(s.path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	check(err)
+
+	defer f.Close()
+
+	writer := bufio.NewWriter(f)
+
+	for _, entry := range entries {
+		err := binary.Write(writer, binary.LittleEndian, uint32(len(entry.Key)))
+		check(err)
+
+		err = binary.Write(writer, binary.LittleEndian, entry.Key)
+		check(err)
+
+		err = binary.Write(writer, binary.LittleEndian, uint32(len(entry.Value)))
+		check(err)
+
+		err = binary.Write(writer, binary.LittleEndian, entry.Value)
+		check(err)
+
+		s.bloomFilter.Add(entry.Key)
+	}
+
+	writer.Flush()
 }
