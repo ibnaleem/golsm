c564ee2d423e0169c0e991862ec4bdbf669adb16
ibnaleem <ibnaleem@outlook.com>
2026-03-08 22:35:02 +0000

    feat(sstable): add IndexEntry struct with Key and Offset fields

--- sstable/sstable.go
+++ sstable/sstable.go
@@ -18,6 +18,11 @@ func check(e error) {
     }
 }
 
+type IndexEntry struct {
+    Key    []byte
+    Offset uint64
+}
+
 type SSTable struct {
 	filepath      string
 	indexFilePath string
@@ -73,4 +78,22 @@ func (s *SSTable) Write(entries []memtable.Entry) {
 	}
 
 	writer.Flush()
+}
+
+func (s *SSTable) Read(key []byte) []byte {
+	if (s.bloomFilter.Test(key)) == false {
+		return nil
+	} else {
+
+		f, err := os.OpenFile(s.filepath, os.O_RDONLY, 0644)
+		check(err)
+
+		defer f.Close()
+
+		reader := bufio.NewReader(f)
+
+
+
+
+	}
 }
