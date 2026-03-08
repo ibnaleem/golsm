4ba3a4aad0a3b59693bcc278a9ebe05169f5e919
ibnaleem <ibnaleem@outlook.com>
2026-03-08 20:19:03 +0000

    feat(sstable): add New constructor with bloom filter and timestamp

--- sstable/sstable.go
+++ sstable/sstable.go
@@ -11,3 +11,19 @@ type SSTable struct {
 	timestamp   time.Time
 	bloomFilter *bloom.BloomFilter
 }
+
+func New(path string) *SSTable {
+	now := time.Now()
+	
+	n := uint(1000000)
+	fp := 0.01 // 1% false positive rate
+
+	bf := bloom.NewWithEstimates(n, fp)
+
+	return &SSTable{
+		path: path,
+		timestamp: now,
+		bloomFilter: bf,
+	}
+
+}
